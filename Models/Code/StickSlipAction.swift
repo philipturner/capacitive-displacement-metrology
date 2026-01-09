870e71cb619c1d65535ddd8f992695722180193a
Philip Turner <philipturner.AR@gmail.com>
2026-01-09 11:07:45 -0500

    Save progress

--- Models/Code/StickSlipAction.swift
+++ Models/Code/StickSlipAction.swift
@@ -12,6 +12,7 @@
 // combinatorial space:
 //   μ_k ∈ {0.3, 0.4, 0.5}
 //   Δv_thres ∈ {1e-5, 1e-4, 1e-3, 1e-2} m/s
+//   gravity ∈ {positive, negative} direction
 //
 // exemplary velocities:
 //   coarse approach woodpecker algorithm: 1.47 μm/s
@@ -22,16 +23,30 @@ struct System {
   // Allowed range: -425 V to 425 V
   var controlVoltage: Float = .zero
   
+  static let piezoConstant: Float = 80e-12 * 6
+  static let piezoMass: Float = 3 * 1.02e-3
   var piezoPosition: Float = .zero
+  static let piezoStiffness: Float = 1.47e9
+  var piezoVelocity: Float = .zero
+  
+  static let sliderMass: Float = 8.94e-3
+  // sliderVelocity = piezoVelocity
   
   var piezoForce: Float {
-    let expectedPosition = controlVoltage * 80e-12 * 6
+    let expectedPosition = controlVoltage * System.piezoConstant
     let remainingDistance = expectedPosition - piezoPosition
-    return 1.47e9 * remainingDistance
+    return System.piezoStiffness * remainingDistance
+  }
+  
+  // No friction force yet (which derives from magnetic normal force)
+  // No gravitational force yet (where sign matters)
+  mutating func integrate(timeStep: Float) {
+    let engagedMass: Float = System.piezoMass + System.sliderMass
+    
   }
 }
 
 var system = System()
 system.controlVoltage = 425
-system.piezoPosition = -425 * 80e-12 * 6
+system.piezoPosition = -425 * System.piezoConstant
 print(system.piezoForce)
