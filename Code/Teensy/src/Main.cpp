4dfdbd068d84f9ba399aaa3326b3983899aaefde
Philip Turner <philipturner.AR@gmail.com>
2026-04-01 11:30:09 -0400

    Working on new code

--- Code/Teensy/src/Main.cpp
+++ Code/Teensy/src/Main.cpp
@@ -7,89 +7,51 @@
 #include "Util/Application.h"
 #include "Util/Bitset.h"
 
-TimeStatistics timeStatistics;
-void kilohertzLoop();
-#define KILOHERTZ_LOOP_PERIOD 4
+// Input: progress, 0 to 1
+// Output: interpolation, 0 to 1
+float smoothstep(float progress) {
+  if (progress < 0) {
+    return 0;
+  } else if (progress > 1) {
+    return 1;
+  } else {
+    return 3 * progress * progress - 2 * progress * progress * progress;
+  }
+}
+
+void changeVoltage(float startVoltage, float endVoltage) {
+  constexpr uint32_t stallTimeMicroseconds = 4;
+  constexpr float duration = 10e-3;
+
+  uint32_t startTime = micros();
+  while (true) {
+    delayMicroseconds(stallTimeMicroseconds);
+    uint32_t latestTime = micros();
+    float elapsedTime = float(latestTime - startTime) / float(1e6);
+
+    float timeProgress = elapsedTime / duration;
+    float voltageProgress = smoothstep(timeProgress);
+
+    float voltage = 0;
+    voltage += voltageProgress * endVoltage;
+    voltage += (1 - voltageProgress) * startVoltage;
+    DAC2::writeVoltage(0, voltage);
+
+    if (elapsedTime > duration) {
+      break;
+    }
+  }
+
+}
 
 void setup() {
   Application::setupSerial();
   Application::setupSPI();
   Application::setupI2C();
 
-  KilohertzLoop::initialize(kilohertzLoop, KILOHERTZ_LOOP_PERIOD);
+  
 }
 
 void loop() {
-  delay(500);
-
-  Serial.println();
-  Serial.println(timeStatistics.above1000000us_jumps);
-  Serial.println(timeStatistics.above100000us_jumps);
-  Serial.println(timeStatistics.above10000us_jumps);
-  Serial.println(timeStatistics.above1000us_jumps);
-  Serial.println(timeStatistics.above100us_jumps);
-  Serial.println(timeStatistics.abovePeriod_jumps);
-  Serial.println(timeStatistics.exactlyPeriod_jumps);
-  Serial.println(timeStatistics.underPeriod_jumps);
-  Serial.println(timeStatistics.total_jumps);
-}
-
-float sineWave(float phaseNormalized) {
-  return sin(phaseNormalized * 2 * M_PI);
-}
-
-float squareWave(float phaseNormalized) {
-  if (phaseNormalized < 0.5) {
-    return 1.0;
-  } else {
-    return -1.0;
-  }
-}
-
-float triangleWave(float phaseNormalized) {
-  float progress;
-  if (phaseNormalized < 0.5) {
-    progress = 2 * phaseNormalized;
-  } else {
-    progress = 2 * (1 - phaseNormalized);
-  }
-
-  return 2 * progress - 1;
-}
-
-void kilohertzLoop() {
-  uint32_t previous = KilohertzLoop::previousTimestamp;
-  uint32_t latest = KilohertzLoop::latestTimestamp;
-  uint32_t jumpDuration = latest - previous;
-  timeStatistics.integrate(jumpDuration, KILOHERTZ_LOOP_PERIOD);
-
-  // Calculate the period and phase, in microseconds.
-  float sineFrequency = 7000;
-  uint32_t sinePeriod = uint32_t(float(1e6) / sineFrequency);
-  uint32_t phase = latest % sinePeriod;
-
-  float phaseNormalized = float(phase) / float(sinePeriod);
-  float waveValue;
-  switch ((latest / 1000000) % 4) {
-    case 0:
-    case 1: {
-      waveValue = sineWave(phaseNormalized);
-      break;
-    }
-    case 2: {
-      waveValue = squareWave(phaseNormalized);
-      break;
-    }
-    case 3: {
-      waveValue = triangleWave(phaseNormalized);
-      break;
-    }
-  }
-  float targetValue = 420 * waveValue;
-
-  // Calculate the voltage.
-  float gainFactor = -35.751;
-  float offset = 0.079;
-  float dacValue = (targetValue - offset) / gainFactor;
-  DAC1::writeVoltage(1, dacValue);
+  // Check the voltage waveform on the oscilloscope.
 }
