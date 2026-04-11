8a36761609779ddd6323849b0e789bd16c3783a5
Philip Turner <philipturner.AR@gmail.com>
2026-04-11 19:49:44 -0400

    4 microseconds per iteration is enough

--- Code/Teensy/src/Main.cpp
+++ Code/Teensy/src/Main.cpp
@@ -6,80 +6,42 @@
 #include "Time/TimeStatistics.h"
 #include "Util/Application.h"
 #include "Util/Bitset.h"
+#include "Util/Rickroll.h"
 
-TimeStatistics timeStatistics;
-void kilohertzLoop();
-#define KILOHERTZ_LOOP_PERIOD 4
+float sineFrequency = -1;
+void piezoTone(float frequency, uint32_t duration);
 
 void setup() {
   Application::setupSerial();
   Application::setupSPI();
   Application::setupI2C();
 
-  KilohertzLoop::initialize(kilohertzLoop, KILOHERTZ_LOOP_PERIOD);
+  for (uint32_t i = 0; i < 50; ++i) {
+    piezoTone(1000, 900);
+    delay(100);
+    piezoTone(250, 900);
+    delay(100);
+  }
 }
 
 void loop() {
-  delay(500);
 
-  Serial.println();
-  Serial.println(timeStatistics.above1000000us_jumps);
-  Serial.println(timeStatistics.above100000us_jumps);
-  Serial.println(timeStatistics.above10000us_jumps);
-  Serial.println(timeStatistics.above1000us_jumps);
-  Serial.println(timeStatistics.above100us_jumps);
-  Serial.println(timeStatistics.abovePeriod_jumps);
-  Serial.println(timeStatistics.exactlyPeriod_jumps);
-  Serial.println(timeStatistics.underPeriod_jumps);
-  Serial.println(timeStatistics.total_jumps);
-}
-
-float sineWave(float phaseNormalized) {
-  return sin(phaseNormalized * 2 * M_PI);
-}
-
-float squareWave(float phaseNormalized) {
-  if (phaseNormalized < 0.5) {
-    return 1.0;
-  } else {
-    return -1.0;
-  }
-}
-
-float triangleWave(float phaseNormalized) {
-  float progress;
-  if (phaseNormalized < 0.5) {
-    progress = 2 * phaseNormalized;
-  } else {
-    progress = 2 * (1 - phaseNormalized);
-  }
-
-  return 2 * progress - 1;
 }
 
 void kilohertzLoop() {
-  uint32_t previous = KilohertzLoop::previousTimestamp;
+  if (sineFrequency <= 0) {
+    Serial.println("Invalid arguments.");
+    exit(0);
+  }
+
   uint32_t latest = KilohertzLoop::latestTimestamp;
-  uint32_t jumpDuration = latest - previous;
-  timeStatistics.integrate(jumpDuration, KILOHERTZ_LOOP_PERIOD);
 
   // Calculate the period and phase, in microseconds.
-  float sineFrequency = 1000;
   uint32_t sinePeriod = uint32_t(float(1e6) / sineFrequency);
   uint32_t phase = latest % sinePeriod;
 
   float phaseNormalized = float(phase) / float(sinePeriod);
-  float waveValue;
-  switch ((latest / 1000000) % 2) {
-    case 0: {
-      waveValue = sineWave(phaseNormalized);
-      break;
-    }
-    case 1: {
-      waveValue = 0;
-      break;
-    }
-  }
+  float waveValue = sin(phaseNormalized * 2 * M_PI);
   float targetValue = 420 * waveValue;
 
   // Calculate the voltage.
@@ -87,4 +49,29 @@ void kilohertzLoop() {
   float offset = 0.079;
   float dacValue = (targetValue - offset) / gainFactor;
   DAC1::writeVoltage(1, dacValue);
+}
+
+// frequency: frequency of the tone, in hertz
+// duration: time to play the note, in milliseconds
+void piezoTone(float frequency, uint32_t duration) {
+  if (frequency <= 0) {
+    Serial.println("Invalid arguments.");
+    exit(0);
+  }
+
+  float time = float(millis()) / 1000;
+  Serial.print("tone ");
+  Serial.print(uint32_t(frequency));
+  Serial.print(" Hz started at ");
+  Serial.print(time, 2);
+  Serial.print(" seconds");
+  Serial.println();
+
+  sineFrequency = frequency;
+  KilohertzLoop::initialize(kilohertzLoop, 4);
+
+  delay(duration);
+
+  KilohertzLoop::timer.end();
+  sineFrequency = -1;
 }
