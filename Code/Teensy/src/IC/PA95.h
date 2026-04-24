6acdcb9f066c8d0521a56881d0c2944929bc2db3
Philip Turner <philipturner.AR@gmail.com>
2026-04-24 18:48:18 -0400

    Finished investigating the newly installed PA95 op amps

--- Code/Teensy/src/IC/PA95.h
+++ Code/Teensy/src/IC/PA95.h
@@ -2,9 +2,19 @@
 
 #include <stdint.h>
 
-// DAC channel -> output channel mapping
-// gain and offset constants for each output channel
-
+// Maximum slew rate:
+// 18.6 V/μs (positive)
+// -20.6 V/μs (negative)
+//
+// Expected: 30.0 V/μs
+//
+// This is not variation within a batch; all three op amps have
+// the number above, to within 0.2 V/μs. Perhaps the culprit is using
+// a 10 pF compensation capacitor instead of 4.7 pF recommended on
+// the data sheet. It will be very hard or impossible to desolder
+// the existing capacitors, now that all three op amps are installed.
 struct PA95 {
+  // DAC channel -> output channel mapping
+  // gain and offset constants for each output channel
   static void writeVoltage(uint8_t channelID, float voltage);
 };
--- Code/Teensy/src/Main.cpp
+++ Code/Teensy/src/Main.cpp
@@ -8,34 +8,46 @@
 #include "Util/Application.h"
 #include "Util/Bitset.h"
 
+float toneFrequency = 300;
+float toneBipolarAmplitude = 360;
+uint8_t channelID = 3;
+void kilohertzLoop();
+
 void setup() {
   Application::setupSerial();
   Application::setupSPI();
   Application::setupI2C();
+
+  KilohertzLoop::initialize(kilohertzLoop, 4);
 }
 
-uint32_t periodID = 0;
-
 void loop() {
-  delay(3000);
 
-  /*
-  float voltages[7] = {
-    10, 5, 2.5, 0, -2.5, -5, -10
-  };
+}
 
-  float dacVoltage = voltages[periodID % 7];
-  DAC1::writeVoltage(1, dacVoltage);
-  DAC1::writeVoltage(3, dacVoltage);
-  DAC1::writeVoltage(2, dacVoltage);
-  */
+float squareWave(float phaseNormalized) {
+  if (phaseNormalized < 0.5) {
+    return 1.0;
+  } else {
+    return -1.0;
+  }
+}
 
-  float voltages[7] = {
-    -400, -200, -100, 0, 100, 200, 400
-  };
-  
-  float voltage = voltages[periodID % 7];
-  PA95::writeVoltage(3, voltage);
+void kilohertzLoop() {
+  if (toneFrequency <= 0) {
+    Serial.println("Invalid arguments.");
+    exit(0);
+  }
 
-  periodID += 1;
+  uint32_t latest = KilohertzLoop::latestTimestamp;
+
+  // Calculate the period and phase, in microseconds.
+  uint32_t sinePeriod = uint32_t(float(1e6) / toneFrequency);
+  uint32_t phase = latest % sinePeriod;
+
+  float phaseNormalized = float(phase) / float(sinePeriod);
+  float waveValue = squareWave(phaseNormalized);
+  waveValue *= toneBipolarAmplitude;
+
+  PA95::writeVoltage(channelID, waveValue);
 }
