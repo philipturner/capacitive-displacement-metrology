8694019f21444a414b7ee052e058397e1ca190b1
Philip Turner <philipturner.AR@gmail.com>
2026-04-28 20:08:20 -0400

    Save state of code

--- Code/Teensy/src/Main.cpp
+++ Code/Teensy/src/Main.cpp
@@ -8,8 +8,8 @@
 #include "Util/Application.h"
 #include "Util/Bitset.h"
 
-uint32_t stepCount = 4000;
-float bipolarDriveVoltage = 120;
+uint32_t stepCount = 15000;
+float bipolarDriveVoltage = 15;
 float positiveDriveVoltage = 0;
 
 void setup() {
@@ -55,7 +55,7 @@ void programBody() {
     voltageRamp(positiveDriveVoltage, -positiveDriveVoltage, 22.5e-6);
     #endif
 
-    delayMicroseconds(600);
+    // delayMicroseconds(600);
   }
 
   voltageRamp(-positiveDriveVoltage, 0);
