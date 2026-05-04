b970aaa96a48cb1a0ea5d4de434d5cea6820b30b
Philip Turner <philipturner.AR@gmail.com>
2026-05-04 17:12:35 -0400

    Save progress

--- Code/Teensy/src/Main.cpp
+++ Code/Teensy/src/Main.cpp
@@ -6,15 +6,25 @@
 #include "Util/Application.h"
 
 constexpr uint32_t loopPeriod = 6;
-
-TimeStatistics timeStatistics;
+constexpr uint32_t programTimeMicroseconds = 3000;
+constexpr uint32_t historyLength = programTimeMicroseconds / loopPeriod / 2;
+float dataStream1[historyLength];
+float dataStream2[historyLength];
 
 void kilohertzLoop();
 
 void setup() {
   Application::setupSerial();
   Application::setupSPI();
+
+  for (uint32_t i = 0; i < historyLength; ++i) {
+    dataStream1[i] = 0;
+    dataStream2[i] = 0;
+  }
+
   KilohertzLoop::initialize(kilohertzLoop, loopPeriod);
+  delay((programTimeMicroseconds + 999) / 1000);
+  KilohertzLoop::timer.end();
 }
 
 // MARK: - Process Input
@@ -33,7 +43,6 @@ void loop() {
   Serial.print(" seconds");
 
   Serial.println();
-  timeStatistics.display();
 
   if (Serial.available() > 0) {
     processInput();
@@ -50,12 +59,47 @@ void loop() {
 
 // MARK: - Kilohertz Loop
 
-void kilohertzLoop() {
-  uint32_t previous = KilohertzLoop::previousTimestamp;
-  uint32_t latest = KilohertzLoop::latestTimestamp;
-  uint32_t jumpDuration = latest - previous;
-  timeStatistics.integrate(jumpDuration, loopPeriod);
+float sineWave(float phaseNormalized) {
+  return sin(phaseNormalized * 2 * M_PI);
+}
 
-  DAC2::writeVoltage(0, 0.0);
-  ADC::readVoltage();
+float squareWave(float phaseNormalized) {
+  if (phaseNormalized < 0.5) {
+    return 1.0;
+  } else {
+    return -1.0;
+  }
+}
+
+float triangleWave(float phaseNormalized) {
+  float progress;
+  if (phaseNormalized < 0.5) {
+    progress = 2 * phaseNormalized;
+  } else {
+    progress = 2 * (1 - phaseNormalized);
+  }
+
+  return 2 * progress - 1;
+}
+
+void kilohertzLoop() {
+  uint32_t elapsedTime = KilohertzLoop::latestTimestamp - KilohertzLoop::startTimestamp;
+  uint32_t sinePeriod = 1000; // in microseconds
+  uint32_t phase = elapsedTime % sinePeriod;
+
+  float phaseNormalized = float(phase) / float(sinePeriod);
+  float waveValueNormalized = triangleWave(phaseNormalized);
+  float biasVoltage = 10 * waveValueNormalized;
+  DAC2::writeVoltage(0, biasVoltage);
+
+  if (KilohertzLoop::iterationID % 2 == 0) {
+    auto conversion = ADC::readVoltage();
+    float tiaVoltage = conversion.voltage;
+
+    uint32_t historyIndex = KilohertzLoop::iterationID / 2;
+    if (historyIndex < historyLength) {
+      dataStream1[historyIndex] = biasVoltage;
+      dataStream2[historyIndex] = tiaVoltage;
+    }
+  }
 }
