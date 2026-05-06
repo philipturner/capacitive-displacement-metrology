2cba7ec15a627056d1832c513f29fd833e470aa7
Philip Turner <philipturner.AR@gmail.com>
2026-05-06 11:05:06 -0400

    Save progress

---
+++ Code/SerialInterface/Sources/SerialInterface/Utilities/Noise.swift
@@ -0,0 +1,7 @@
+//
+//  Noise.swift
+//  SerialInterface
+//
+//  Created by Philip Turner on 5/6/26.
+//
+
--- Code/SerialInterface/Sources/SerialInterface/main.swift
+++ Code/SerialInterface/Sources/SerialInterface/main.swift
@@ -6,6 +6,14 @@ PythonLibrary.useLibrary(at: "/Users/philipturner/miniforge3/bin/python")
 let plt = Python.import("matplotlib.pyplot")
 let ticker = Python.import("matplotlib.ticker")
 
+// Mode
+
+enum Mode {
+  case riseTime
+  case noise
+}
+let mode: Mode = .noise
+
 // Access the serial port.
 
 let serial = SerialPort(path: "/dev/cu.usbmodem182280901")
@@ -15,11 +23,21 @@ try await serial.open(
   transmitRate: .baud115200)
 print("opened serial port")
 
-let byte = Character("r").asciiValue!
-_ = try await serial.writeBytes([byte])
-usleep(60_000)
+do {
+  var character: Character
+  switch mode {
+  case .riseTime: character = "r"
+  case .noise: character = "n"
+  }
+  
+  _ = try await serial.writeBytes([character.asciiValue!])
+  usleep(60_000)
+}
 
-let data = try await serial.readBytesBlocking(count: 1_000_000, timeout: 0.001)
+_ = try await serial.writeBytes([Character("e").asciiValue!])
+usleep(60_000 + 50_000)
+
+let data = try await serial.readBytesBlocking(count: 1_000_000, timeout: 0.015)
 let string = String(data: data, encoding: .utf8)!
 let lines = string.split(separator: "\r\n").map(String.init)
 
@@ -36,36 +54,39 @@ guard entries.count > 0 else {
   fatalError("There were no entries.")
 }
 
-// Organize into streams.
-
-let streams = Stream.createStreams(entries)
-let riseTimeStreams = RiseTime.createRiseTimeStreams(streams: streams)
-
 // Graph with Matplotlib.
 
-do {
-  // Retrieve the figure and axes.
-  let (fig, axis) = plt.subplots().tuple2
-  let axes = [axis]
-  
-  // Set the size of the figure.
-  fig.set_size_inches(12, 6)
-  
-  // Plot on the first subplot.
-  axes[0].plot(streams[0].data, streams[1].data, label: streams[1].title)
-  axes[0].plot(streams[0].data, streams[2].data, label: streams[2].title)
+// Retrieve the figure and axes.
+let (fig, axis) = plt.subplots().tuple2
+let axes = [axis]
+
+// Set the size of the figure.
+fig.set_size_inches(12, 6)
+
+// Plot on the first subplot.
+let streams = Stream.createStreams(entries)
+axes[0].plot(streams[0].data, streams[1].data, label: streams[1].title)
+axes[0].plot(streams[0].data, streams[2].data, label: streams[2].title)
+
+if mode == .riseTime {
+  let riseTimeStreams = RiseTime.createRiseTimeStreams(streams: streams)
   axes[0].scatter(
     riseTimeStreams.x.data,
     riseTimeStreams.y.data,
     label: riseTimeStreams.y.title)
-  
-  // Format the subplot.
-  axes[0].set_xlabel(streams[0].title)
+}
+
+// Format the subplot.
+axes[0].set_xlabel(streams[0].title)
+if mode == .riseTime {
   axes[0].xaxis.set_major_locator(ticker.MultipleLocator(500))
   axes[0].xaxis.set_minor_locator(ticker.MultipleLocator(100))
-  axes[0].grid(true)
-  axes[0].grid(visible: true, which: "minor", axis: "x")
-  
-  fig.legend(loc: "outside upper left")
-  plt.show()
 }
+axes[0].grid(true)
+if mode == .riseTime {
+  axes[0].grid(visible: true, which: "minor", axis: "x")
+}
+
+fig.legend(loc: "outside upper left")
+plt.show()
+
