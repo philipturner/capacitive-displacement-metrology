770de14c48fe5847c74ef283d71f4e65cf2aac07
Philip Turner <philipturner.AR@gmail.com>
2026-05-18 10:46:22 -0400

    Save progress

--- Code/Simulation/SecondOrderFilter.swift
+++ Code/Simulation/SecondOrderFilter.swift
@@ -1,23 +1,9 @@
 import Foundation
 
-// Filter parameters
-let resonanceFrequency: Float = 8700 // Hz
-let Q: Float = 200
-let lowpassCutoff: Float = 1500 // Hz
-
-// Signal parameters
-let dacResolution: Int = 12 // μs
-let signalAmplitude: Float = 100
-let riseTime: Int = 100 // μs
-
-enum StepType {
-  case immediate
-  case firstOrderSmooth
-  case secondOrderSmooth
-}
-let stepType: StepType = .immediate
-
 // MARK: - Filters
+
+let resonanceFrequency: Float = 8700
+let Q: Float = 200
 
 struct BiquadFilterDescriptor {
   var resonanceFrequency: Float?
@@ -119,18 +105,17 @@ func createBiquadFilter() -> BiquadFilter {
   filterDesc.Q = Q
   return BiquadFilter(descriptor: filterDesc)
 }
-var biquadFilter = createBiquadFilter()
 
-func createFirstOrderFilter() -> FirstOrderFilter {
+func createFirstOrderFilter(cutoff: Float) -> FirstOrderFilter {
   var filterDesc = FirstOrderFilterDescriptor()
-  filterDesc.cutoffFrequency  = lowpassCutoff
+  filterDesc.cutoffFrequency = cutoff
   filterDesc.samplingFrequency = 1_000_000
   return FirstOrderFilter(descriptor: filterDesc)
 }
-var firstOrderFilter = createFirstOrderFilter()
+
+// MARK: - Signal Generation
 
 // Perfect impulse: 67 ms for +100 nm -> +/-10 pm (initial overshoot to 200 nm)
-// Sine waves have +/-100 nm amplitude
 //
 // Smooth DAC waveform
 // Smoothstep, 1st order: 61 ms @ 100 μs (initial overshoot to 142 nm)
@@ -148,17 +133,13 @@ var firstOrderFilter = createFirstOrderFilter()
 //
 // Steppy DAC waveform (12 μs)
 // Smoothstep, 1st order: 100 μs -> initial overshoot to 141 nm
-// Smoothstep, 1st order: 1000 μs -> initial overshoot to
-// Smoothstep, 1st order: 2000 μs -> initial overshoot to
-// Smoothstep, 1st order: 3000 μs -> initial overshoot to
-// Smoothstep, 1st order: 4000 μs -> initial overshoot to
-// Smoothstep, 1st order: 5000 μs -> initial overshoot to
-// Smoothstep, 2nd order: 100 μs -> initial overshoot to
-// Smoothstep, 2nd order: 1000 μs -> initial overshoot to
-// Smoothstep, 2nd order: 2000 μs -> initial overshoot to
-// Smoothstep, 2nd order: 3000 μs -> initial overshoot to
-// Smoothstep, 2nd order: 4000 μs -> initial overshoot to
-// Smoothstep, 2nd order: 5000 μs -> initial overshoot to
+
+enum StepType {
+  case immediate
+  case firstOrderSmooth
+  case secondOrderSmooth
+}
+let stepType: StepType = .firstOrderSmooth
 
 func smoothstep(progress: Float) -> Float {
   if progress <= 0 {
@@ -182,30 +163,82 @@ func smoothstep(progress: Float) -> Float {
   }
 }
 
+// unit: μs
+let dacResolution: Int = 12
+
+// unit: μs
+let riseTimes: [Int] = [
+  100, 1000, 5000,
+//  1000
+]
+
 // MARK: - Simulation
 
-for t in 0..<1_000 {
-  let steppedTime = t - (t % dacResolution)
-  let progress = Float(steppedTime) / Float(riseTime)
-  let signal = signalAmplitude * smoothstep(progress: progress)
-  let filtered = biquadFilter.update(input: signal)
-  let filtered2 = firstOrderFilter.update(input: filtered)
+var trialResults: [SIMD3<Float>] = []
+for trialID in riseTimes.indices {
+  var biquadFilter = createBiquadFilter()
+  var lowpassFilter38 = createFirstOrderFilter(cutoff: 3800)
+  var lowpassFilter15 = createFirstOrderFilter(cutoff: 1500)
   
-  func format(_ number: Float) -> String {
-    var output = String(format: "%.3f", number)
-    
-    let exampleString = "-XXX.XXX"
-    while output.count < exampleString.count {
-      output = " " + output
+  let riseTime = riseTimes[trialID]
+  
+  func createSimulationTime() -> Int {
+    var output: Int = .zero
+    output += dacResolution
+    if stepType != .immediate {
+      output += riseTime
     }
+    output += 2 * (1_000_000 / 1500)
+    output += 3 * Int(1e6 / resonanceFrequency)
     return output
   }
   
-  if t % 1 == 0 {
-    print("t = \(t) μs", terminator: " | ")
-    print("signal = \(format(signal))", terminator: " | ")
-    print("filtered = \(format(filtered))", terminator: " | ")
-    print("filtered2 = \(format(filtered2))", terminator: " | ")
-    print()
+  var maxAmplitudes: SIMD3<Float> = .zero
+  for t in 0..<createSimulationTime() {
+    let dacApparentTime = t - (t % dacResolution)
+    let progress = Float(dacApparentTime) / Float(riseTime)
+    let signal = 1 * smoothstep(progress: progress)
+    
+    let filtered = biquadFilter.update(input: signal)
+    let filtered38 = lowpassFilter38.update(input: filtered)
+    let filtered15 = lowpassFilter15.update(input: filtered)
+    let amplitudes = SIMD3(filtered, filtered38, filtered15)
+    maxAmplitudes.replace(
+      with: amplitudes,
+      where: amplitudes .> maxAmplitudes)
+    
+    func format(_ number: Float) -> String {
+      var output = String(format: "%.5f", number)
+      
+      let exampleString = "-X.XXXXX"
+      while output.count < exampleString.count {
+        output = " " + output
+      }
+      return output
+    }
+    
+    if t % 1 == 0 {
+      print("t = \(t) μs", terminator: " | ")
+      print("signal = \(format(signal))", terminator: " | ")
+      print("biquad = \(format(filtered))", terminator: " | ")
+      print("3.8k = \(format(filtered38))", terminator: " | ")
+      print("1.5k = \(format(filtered15))", terminator: " | ")
+      print()
+    }
   }
+  
+  let overshootProportions = maxAmplitudes - 1
+  trialResults.append(overshootProportions)
+}
+
+print()
+for trialID in riseTimes.indices {
+  let riseTime = riseTimes[trialID]
+  let overshootProportions = trialResults[trialID]
+  
+  print(riseTime, terminator: ", ")
+  print(overshootProportions[0], terminator: ", ")
+  print(overshootProportions[1], terminator: ", ")
+  print(overshootProportions[2], terminator: ", ")
+  print()
 }
